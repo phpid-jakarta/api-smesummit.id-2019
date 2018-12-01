d0f7e0985b267bb9111a0d2f6358dda3acf73aab
Ammar Faizi <ammarfaizi2@gmail.com>
2018-12-02 01:04:30 +0700

    Prepare participant register API test

---
+++ tests/API/ParticipantRegisterTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace tests\API;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class ini digunakan untuk mengetest API /participant_register.php
+ *
+ * @link https://en.wikipedia.org/wiki/Singleton_pattern
+ *
+ * @author Ammar Faizi <ammarfaizi2@gmail.com> https://www.facebook.com/ammarfaizi2
+ * @license MIT
+ * @package \Encryption
+ */
