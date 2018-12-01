e3039d33a3cc9b4aa3efadb27e912579c4711a88
Ammar Faizi <ammarfaizi2@gmail.com>
2018-12-01 21:42:38 +0700

    Prepare test case

---
+++ tests/Encryption/CencryptIntegrityTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace tests\Encryption;
+
+use Encryption\Cencrypt;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class ini digunakan untuk mengetest integritas data pada encryption.
+ *
+ * @link https://en.wikipedia.org/wiki/Singleton_pattern
+ *
+ * @author Ammar Faizi <ammarfaizi2@gmail.com> https://www.facebook.com/ammarfaizi2
+ * @license MIT
+ * @package \Encryption
+ */
+class CencryptIntegrityTest extends TestCase
+{
+	/**
+	 * @return void
+	 */
+	public function test1(): void
+	{
+		for ($i=1; $i <= 10000; $i++) { 
+			$str = rstr($i);
+			$key = rstr(32);
+
+			$encrypted = Cencrypt::encrypt($str, $key);
+
+			$this->assertEquals(
+				Cencrypt::decrypt($encrypted, $key),
+				$str
+			);
+		}
+	}
+}
