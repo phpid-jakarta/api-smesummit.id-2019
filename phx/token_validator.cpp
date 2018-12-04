3b5e9126d06e49893a66b7330dde096be9a74c5d
Ammar Faizi <ammarfaizi2@gmail.com>
2018-12-04 12:51:22 +0700

    [a01] Update some cpp files

--- phx/token_validator.cpp
+++ phx/token_validator.cpp
@@ -2,6 +2,11 @@
 #include "headers/token_validator.hpp"
 #include "headers/token_validator_class.hpp"
 
-Php::Value validate_token(Php::Parameters &p) {
-	Php::Value decrypted_token = Php::call("dencrypt", );
+Php::Value
+validate_token(Php::Parameters &p) {
+	token_validator *validator = new token_validator(p[0]);
+	int r = validator->validate();
+	free(validator);
+	validator = nullptr;
+	return r;
 }
