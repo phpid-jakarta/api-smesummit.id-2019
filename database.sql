ec61877cbf6a157e9d8da186e54051dd96ff74d6
Ammar Faizi <ammarfaizi2@gmail.com>
2018-12-29 15:38:16 +0700

    [master] Update DB

--- database.sql
+++ database.sql
@@ -5,6 +5,8 @@ SET time_zone = '+00:00';
 SET foreign_key_checks = 0;
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
+SET NAMES utf8mb4;
+
 DROP TABLE IF EXISTS `coachers`;
 CREATE TABLE `coachers` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -30,7 +32,7 @@ CREATE TABLE `coachers` (
   KEY `company_sector` (`sector`),
   KEY `topic` (`topic`),
   FULLTEXT KEY `experience` (`experience`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
 DROP TABLE IF EXISTS `faq`;
@@ -39,20 +41,17 @@ CREATE TABLE `faq` (
   `question` text NOT NULL,
   `answer` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `faq` (`id`, `question`, `answer`) VALUES
-(1, 'What is <b>SME Summit</b>?', 'SME Summit 2019 is a conference and mentoring panel session organized by PHP Indonesia which aims to help the technological transformation in non-tech companies, small and medium enterprises and traditional companies, shift the paradigm that IT division is only as a support division in the company into a profit-center division and helping to transform your stagnant companies into exponential growth companies.'),
-(2, 'I am a question blablabla blablabla?', 'I am the answer <b>qweqweqwe</b>');
 
 DROP TABLE IF EXISTS `participants`;
 CREATE TABLE `participants` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `company_name` varchar(255) NOT NULL,
-  `position` varchar(255) NOT NULL,
-  `coached_sector` varchar(255) NOT NULL,
   `company_sector` varchar(255) NOT NULL,
+  `position` varchar(255) NOT NULL,
+  `sector_to_be_coached` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `phone` varchar(255) NOT NULL,
   `problem_desc` text NOT NULL,
@@ -60,11 +59,11 @@ CREATE TABLE `participants` (
   PRIMARY KEY (`id`),
   KEY `name` (`name`),
   KEY `company_name` (`company_name`),
-  KEY `position` (`position`),
-  KEY `company_sector` (`company_sector`),
+  KEY `position` (`company_sector`),
+  KEY `company_sector` (`sector_to_be_coached`),
   KEY `email` (`email`),
   FULLTEXT KEY `problem_desc` (`problem_desc`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
 DROP TABLE IF EXISTS `speakers`;
@@ -96,11 +95,8 @@ CREATE TABLE `speakers` (
   KEY `phone` (`phone`),
   FULLTEXT KEY `experience` (`experience`),
   FULLTEXT KEY `topic` (`topic`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `speakers` (`id`, `name`, `company_name`, `position`, `email`, `photo`, `last_education`, `experience`, `phone`, `sector`, `topic`, `fb_link`, `twitter_link`, `instagram_link`, `promoted`, `created_at`) VALUES
-(1, 'Ken Ratri Iswari', 'Geekhunter', 'Founder & CEO Geekhunter', NULL, 'https://www.smesummit.id/images/speaker/ken-ratri-iswari.jpeg',  NULL, NULL, NULL, 'Human Asset Sector', NULL, 'https://www.facebook.com/kenratriiswari',  'https://twitter.com/kenratriiswari', 'https://www.instagram.com/kenratriiswari/',  '1',  '2018-12-21 20:40:42'),
-(2, 'Rendy Maulana',  'Qwords', 'CEO of Qwords',  NULL, 'https://www.smesummit.id/images/speaker/rendy-maulana.jpeg', NULL, NULL, NULL, 'Regulation Sector',  NULL, 'https://www.facebook.com/rendy.maulana', '', 'https://www.linkedin.com/in/rendy/', '1',  '2018-12-21 20:41:21');
 
 DROP TABLE IF EXISTS `sponsors`;
 CREATE TABLE `sponsors` (
@@ -117,7 +113,7 @@ CREATE TABLE `sponsors` (
   KEY `email_pic` (`email_pic`),
   KEY `phone` (`phone`),
   KEY `sponsor_type` (`sponsor_type`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
 DROP TABLE IF EXISTS `volunteers`;
@@ -133,7 +129,7 @@ CREATE TABLE `volunteers` (
   KEY `email` (`email`),
   KEY `phone` (`phone`),
   FULLTEXT KEY `why_you_apply_desc` (`why_you_apply_desc`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 
--- 2018-12-21 13:41:45
+-- 2018-12-29 08:38:00
