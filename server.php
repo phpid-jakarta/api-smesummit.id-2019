570c0722017753062f6454b8e780fd0c1912796a
Ammar Faizi <ammarfaizi2@gmail.com>
2019-01-29 16:46:51 +0700

    Add display_errors flag

--- server.php
+++ server.php
@@ -4,7 +4,7 @@ declare(ticks=1);
 
 $port = "8080";
 $docRoot = __DIR__."/public";
-// $extArgv = "";
+$extArgv = "";
 // $extArgv = "-d extension=".__DIR__."/shared_objects/apismesummit_ext1.so";
 
 function deletePidFile()
@@ -26,7 +26,7 @@ $fileDescriptor = [
 file_put_contents(__DIR__."/php_server.pid", getmypid());
 
 $docRoot = escapeshellarg($docRoot);
-$cmd = PHP_BINARY." {$extArgv} -S 0.0.0.0:{$port} -t {$docRoot}";
+$cmd = PHP_BINARY." -d display_errors=1 {$extArgv} -S 0.0.0.0:{$port} -t {$docRoot}";
 print $cmd.PHP_EOL.PHP_EOL;
 $res = proc_open($cmd, $fileDescriptor, $pipes);
 proc_close($res);
