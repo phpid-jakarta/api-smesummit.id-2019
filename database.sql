60adb8cf1dd087006dc4b53e8e5202160c5ccb79
Ammar Faizi <ammarfaizi2@gmail.com>
2019-01-29 17:08:17 +0700

    Update database.sql

--- database.sql
+++ database.sql
@@ -20,17 +20,18 @@ CREATE TABLE `coachers` (
   `phone` varchar(32) NOT NULL,
   `sector` varchar(255) NOT NULL,
   `topic` text NOT NULL,
+  `status` enum('New Request','Follow Up','Confirmed','Canceled','Rejected') NOT NULL DEFAULT 'New Request',
   `created_at` datetime NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `name` (`name`),
-  KEY `company_name` (`company_name`),
-  KEY `position` (`position`),
-  KEY `email` (`email`),
-  KEY `photo` (`photo`),
+  KEY `name` (`name`(191)),
+  KEY `company_name` (`company_name`(191)),
+  KEY `position` (`position`(191)),
+  KEY `email` (`email`(191)),
+  KEY `photo` (`photo`(191)),
   KEY `latest_education` (`last_education`),
   KEY `phone` (`phone`),
-  KEY `company_sector` (`sector`),
-  KEY `topic` (`topic`(255)),
+  KEY `company_sector` (`sector`(191)),
+  KEY `topic` (`topic`(191)),
   FULLTEXT KEY `experience` (`experience`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -41,7 +42,7 @@ CREATE TABLE `faq` (
   `question` text NOT NULL,
   `answer` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
 DROP TABLE IF EXISTS `participants`;
@@ -55,6 +56,7 @@ CREATE TABLE `participants` (
   `email` varchar(255) NOT NULL,
   `phone` varchar(255) NOT NULL,
   `problem_desc` text NOT NULL,
+  `status` enum('New Request','Invoice Sent','Confirmed','Canceled','Rejected') DEFAULT NULL,
   `created_at` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `name` (`name`),
@@ -63,7 +65,27 @@ CREATE TABLE `participants` (
   KEY `company_sector` (`sector_to_be_coached`),
   KEY `email` (`email`),
   FULLTEXT KEY `problem_desc` (`problem_desc`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+DROP TABLE IF EXISTS `payment_confirmation`;
+CREATE TABLE `payment_confirmation` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `email_user_id` bigint(20) DEFAULT NULL,
+  `phone` varchar(255) NOT NULL,
+  `total_payment` varchar(255) NOT NULL,
+  `payment_type` enum('participant','sponsor','coacher') NOT NULL,
+  `date_transfer` varchar(64) NOT NULL,
+  `no_ref` varchar(255) NOT NULL,
+  `bank_name` varchar(64) NOT NULL,
+  `bank_username` varchar(64) NOT NULL,
+  `screenshot` text NOT NULL,
+  `status` enum('pending','approved','rejected') NOT NULL,
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `email_user_id` (`email_user_id`),
+  CONSTRAINT `payment_confirmation_ibfk_2` FOREIGN KEY (`email_user_id`) REFERENCES `participants` (`id`) ON DELETE SET NULL ON UPDATE SET NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
 DROP TABLE IF EXISTS `speakers`;
@@ -83,6 +105,7 @@ CREATE TABLE `speakers` (
   `twitter_link` varchar(255) DEFAULT NULL,
   `instagram_link` varchar(255) DEFAULT NULL,
   `promoted` enum('0','1') NOT NULL DEFAULT '0',
+  `status` enum('New Request','Follow Up','Confirmed','Canceled','Rejected') DEFAULT 'New Request',
   `created_at` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `name` (`name`),
@@ -95,7 +118,7 @@ CREATE TABLE `speakers` (
   KEY `phone` (`phone`),
   FULLTEXT KEY `experience` (`experience`),
   FULLTEXT KEY `topic` (`topic`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
 DROP TABLE IF EXISTS `sponsors`;
@@ -107,11 +130,16 @@ CREATE TABLE `sponsors` (
   `company_logo` varchar(255) NOT NULL,
   `phone` varchar(32) NOT NULL,
   `sponsor_type` enum('platinum','gold','silver','media_partner') NOT NULL,
+  `status` enum('New Request','Invoice Sent','Confirmed','Canceled','Rejected') NOT NULL DEFAULT 'New Request',
+  `remarks` varchar(500) DEFAULT NULL,
+  `payment_amount` decimal(15,2) DEFAULT '0.00',
+  `payment_date` date DEFAULT NULL,
+  `payment_reference` varchar(255) DEFAULT NULL,
   `created_at` datetime NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `company_name` (`company_name`),
-  KEY `company_sector` (`company_sector`),
-  KEY `email_pic` (`email_pic`),
+  KEY `company_name` (`company_name`(191)),
+  KEY `company_sector` (`company_sector`(191)),
+  KEY `email_pic` (`email_pic`(191)),
   KEY `phone` (`phone`),
   KEY `sponsor_type` (`sponsor_type`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -125,21 +153,16 @@ CREATE TABLE `volunteers` (
   `phone` varchar(32) NOT NULL,
   `why_you_apply_desc` text NOT NULL,
   `created_at` datetime NOT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `status` enum('New Request','Follow Up','Confirmed','Canceled','Rejected') DEFAULT 'New Request',
+  `ig_link` varchar(255) DEFAULT NULL,
+  `fb_link` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `name` (`name`),
   KEY `email` (`email`),
   KEY `phone` (`phone`),
   FULLTEXT KEY `why_you_apply_desc` (`why_you_apply_desc`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
-ALTER TABLE `volunteers`
-ADD `city` varchar(255) DEFAULT NULL;
-
-ALTER TABLE `volunteers`
-ADD `ig_link` varchar(255) DEFAULT NULL;
-
-ALTER TABLE `volunteers`
-ADD `fb_link` varchar(255) DEFAULT NULL;
-
--- 2019-01-07 17:29:59
+-- 2019-01-29 10:07:23
