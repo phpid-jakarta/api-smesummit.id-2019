245675a10f5218ec3f45af84fbe63f4975786c2a
Ammar Faizi <ammarfaizi2@gmail.com>
2019-04-09 13:45:05 +0700

    Fix ticket generator

---
+++ cli/create_ticket.php
@@ -0,0 +1,22 @@
+<?php
+
+require __DIR__."/../bootstrap/init.php";
+require __DIR__."/../vendor/autoload.php";
+
+$pdo = DB::pdo();
+$st = $pdo->prepare("SELECT `a`.`name`,`a`.`company_name`,`a`.`email`,`a`.`phone`,CONCAT('par',`b`.`ticket_code`) AS `ticket_code` FROM `participants` AS `a` INNER JOIN `participants_ticket` AS `b` ON `a`.`id` = `b`.`participant_id`;");
+$st->execute();
+while ($r = $st->fetch(PDO::FETCH_ASSOC)) {
+	print json_encode($r)."\n";
+	// $barcode = new Picqer\Barcode\BarcodeGeneratorPNG();
+	// $qrCode = new Endroid\QrCode\QrCode($r["ticket_code"]);
+
+	// file_put_contents(
+	// 	BASEPATH."/storage/tickets/qrcode/{$r["ticket_code"]}.png",
+	// 	$qrCode->writeString()
+	// );
+	// file_put_contents(
+	// 	BASEPATH."/storage/tickets/barcode/{$r["ticket_code"]}.png",
+	// 	$barcode->getBarcode($r["ticket_code"], $barcode::TYPE_CODE_128)
+	// );
+}
--- cli/gen_ticket.php
+++ cli/gen_ticket.php
@@ -5,15 +5,15 @@ require __DIR__."/../bootstrap/init.php";
 $pdo = DB::pdo();
 $st = $pdo->prepare("SELECT * FROM `participants`;");
 $st->execute();
-$query = "INSERT INTO `participants_ticket` (`participant_id`, `ticket_id`, `created_at`) VALUES ";
+$query = "INSERT INTO `participants_ticket` (`participant_id`, `ticket_code`, `created_at`) VALUES ";
 $now = date("Y-m-d H:i:s");
 $i = 0;
 $d[":created_at"] = $now;
 while ($r = $st->fetch(PDO::FETCH_ASSOC)) {
 	$id = sprintf("%04x\n", $r["id"]);
-	$query .= "(:participant_id{$i}, :ticket_id{$i}, :created_at),";
+	$query .= "(:participant_id{$i}, :ticket_code{$i}, :created_at),";
 	$d[":participant_id{$i}"] = $r["id"];
-	$d[":ticket_id{$i}"] = $id;
+	$d[":ticket_code{$i}"] = $id;
 	$i++;
 }
 $query = rtrim($query, ",");
