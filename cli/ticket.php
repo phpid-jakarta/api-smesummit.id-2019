f98775d6b9bbbedfc450fe9804639ee9dfaa8684
Ammar Faizi <ammarfaizi2@gmail.com>
2019-04-09 13:25:58 +0700

    Prepare ticket

---
+++ cli/ticket.php
@@ -0,0 +1,22 @@
+<?php
+
+require __DIR__."/../bootstrap/init.php";
+
+$pdo = DB::pdo();
+$st = $pdo->prepare("SELECT * FROM `participants`;");
+$st->execute();
+$query = "INSERT INTO `participants_ticket` (`participant_id`, `ticket_id`, `created_at`) VALUES ";
+$now = date("Y-m-d H:i:s");
+$i = 0;
+$d[":created_at"] = $now;
+while ($r = $st->fetch(PDO::FETCH_ASSOC)) {
+	$id = sprintf("%04x\n", $r["id"]);
+	$query .= "(:participant_id{$i}, :ticket_id{$i}, :created_at),";
+	$d[":participant_id{$i}"] = $r["id"];
+	$d[":ticket_id{$i}"] = $id;
+	$i++;
+}
+$query = rtrim($query, ",");
+$st = $pdo->prepare($query);
+$st->execute($d);
+unset($pdo);
